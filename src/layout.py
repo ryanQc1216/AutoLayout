4f4973ebf6ad980f608e733474c3486cd4bba9c9
ryanQc1216 <qiancheng1216@163.com>
2021-02-12 14:34:33 +0800

    update algo

--- src/layout.py
+++ src/layout.py
@@ -3,8 +3,9 @@ import cv2
 import json
 import copy
 import numpy as np
-from src.node import Coordinate, Node
+from src.node import Coordinate, Node, Group
 from src.render import Render
+
 
 def parsing_children_description(description):
     if 'children' not in description:
@@ -17,9 +18,7 @@ class Layout:
         self.description = description
         self.max_node_support = 999
         self.start_parent_id = -1
-        self.maps, self.raw_groups = self.translate_maps()
-        self.refine_groups()
-        self.group = copy.deepcopy(self.raw_groups)
+        self.maps, self.groups = self.translate_maps()
         self.calc_canvas()
 
     def translate_maps(self):
@@ -31,7 +30,10 @@ class Layout:
                           children=children_node_id,
                           parent=self.start_parent_id,
                           group_id=0)
-        groups[self.start_parent_id] = {'group_id': 0, 'contains': [self.description['id']], 'layer_id': 0}
+        groups[self.start_parent_id] = Group(contains=[self.description['id']],
+                                             group_id=0,
+                                             layer_id=0,
+                                             parent_node_id=self.start_parent_id)
         maps[start_node.node_id] = start_node
         candidates = children_description.copy()
 
@@ -43,9 +45,10 @@ class Layout:
             parent_node_id = candidate['parentId']
             parent_layer_id = maps[parent_node_id].layer_id
             if parent_node_id not in groups:
-                groups[parent_node_id] = {'group_id': len(groups),
-                                          'contains': maps[parent_node_id].children.copy(),
-                                          'layer_id': parent_layer_id + 1}
+                groups[parent_node_id] = Group(contains=maps[parent_node_id].children.copy(),
+                                               group_id=len(groups),
+                                               layer_id=parent_layer_id + 1,
+                                               parent_node_id=parent_node_id)
             else:
                 pass
             this_node = Node(node_id=node_id,
@@ -58,74 +61,66 @@ class Layout:
             _iter += 1
             assert _iter < self.max_node_support, 'Surpass the max node number limit'
 
-        # check groups contain equal to maps
+        # check groups contain equal to maps , and assign
         cnt = 0
         for p in groups:
-            cnt += len(groups[p]['contains'])
+            groups[p].calc_relative_coord(maps)
+            cnt += len(groups[p].contains)
         assert cnt == len(maps), 'translate_maps error happened!!'
         return maps, groups
 
-    # if one group has only one nodes
-    def refine_groups(self):
-
-        pass
-
-    def get_text_size(self):
-        text_size, baseline = cv2.getTextSize('1111', fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1.0, thickness=1)
-        return text_size
-
-    def calc_groups_bbox(self):
-
-        pass
-
-    def check_all_node_position_assigned(self):
+    def check_all_node_relative_assigned(self):
         cnt_assigned = 0
         for node_id in self.maps:
-            if self.maps[node_id].absolute_coord.x is not None and self.maps[node_id].absolute_coord.y is not None:
+            if self.maps[node_id].relative_coord.x is not None and self.maps[node_id].relative_coord.y is not None:
                 cnt_assigned += 1
         if cnt_assigned == len(self.maps):
             return True
         else:
             return False
 
-    def calc_pos_by_parent(self, layer_step, node_step, parent_node_id, contains):
-        parent_x = self.maps[parent_node_id].absolute_coord.x
-        parent_y = self.maps[parent_node_id].absolute_coord.y
-        width = (len(contains) - 1) * node_step
-        start_x = - width / 2.0
-        positions = []
-        for idx in range(len(contains)):
-            temp_x = parent_x + start_x + idx * node_step
-            temp_y = parent_y + layer_step
-            positions.append((int(temp_x), int(temp_y)))
-        return positions
-
     def calc_canvas(self):
-        maps = self.maps
-        groups = self.raw_groups
-        text_size = self.get_text_size()
-        layer_step = text_size[0] * 2
-        node_step = text_size[0] * 2
-
-        # assign first node's position
-        start_node_id = groups[self.start_parent_id]['contains'][0]
-        maps[start_node_id].assign_absolute_position(x=0, y=0)
-
-        while self.check_all_node_position_assigned() is False:
-            for parent_node_id in groups:
-                if parent_node_id == self.start_parent_id:
-                    continue
-                if self.maps[parent_node_id].valid_absolute_coord() is False:
-                    continue
-                contains = groups[parent_node_id]['contains']
-                positions = self.calc_pos_by_parent(layer_step, node_step, parent_node_id, contains)
-                for idx in range(len(contains)):
-                    node_id = contains[idx]
-                    self.maps[node_id].assign_absolute_position(x=positions[idx][0], y=positions[idx][1])
-
-        ins_render = Render(self.maps)
-        ins_render.render()
+        assert self.check_all_node_relative_assigned()
 
         pass
 
 
+    # def calc_pos_by_parent(self, layer_step, node_step, parent_node_id, contains):
+    #     parent_x = self.maps[parent_node_id].absolute_coord.x
+    #     parent_y = self.maps[parent_node_id].absolute_coord.y
+    #     width = (len(contains) - 1) * node_step
+    #     start_x = - width / 2.0
+    #     positions = []
+    #     for idx in range(len(contains)):
+    #         temp_x = parent_x + start_x + idx * node_step
+    #         temp_y = parent_y + layer_step
+    #         positions.append((int(temp_x), int(temp_y)))
+    #     return positions
+    #
+    # def calc_canvas(self):
+    #     maps = self.maps
+    #     groups = self.raw_groups
+    #     text_size = self.get_text_size()
+    #     layer_step = text_size[0] * 2
+    #     node_step = text_size[0] * 2
+    #
+    #     # assign first node's position
+    #     start_node_id = groups[self.start_parent_id].contains[0]
+    #     maps[start_node_id].assign_absolute_coord(x=0, y=0)
+    #
+    #     while self.check_all_node_position_assigned() is False:
+    #         for parent_node_id in groups:
+    #             if parent_node_id == self.start_parent_id:
+    #                 continue
+    #             if self.maps[parent_node_id].valid_absolute_coord() is False:
+    #                 continue
+    #             contains = groups[parent_node_id].contains
+    #             positions = self.calc_pos_by_parent(layer_step, node_step, parent_node_id, contains)
+    #             for idx in range(len(contains)):
+    #                 node_id = contains[idx]
+    #                 self.maps[node_id].assign_absolute_coord(x=positions[idx][0], y=positions[idx][1])
+    #
+    #     ins_render = Render(self.maps)
+    #     ins_render.render()
+    #
+    #     pass
--- src/node.py
+++ src/node.py
@@ -4,6 +4,14 @@ import json
 import copy
 import numpy as np
 
+
+
+def get_default_text_size():
+    text_size, baseline = cv2.getTextSize('0000', fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1.0, thickness=1)
+    text_width = text_size[0]
+    text_height = text_size[1]
+    return text_width, text_height
+
 class Coordinate:
     def __init__(self, x=None, y=None):
         self.x = x
@@ -22,15 +30,46 @@ class Node:
         self.parent = parent
         self.group_id = group_id
         self.absolute_coord = Coordinate()
-        # self.Relative_x = None
+        self.relative_coord = Coordinate()
+
+    def assign_absolute_coord(self, x, y):
+        self.absolute_coord.x = x
+        self.absolute_coord.y = y
 
     def valid_absolute_coord(self):
         if self.absolute_coord.x is None or self.absolute_coord.y is None:
             return False
-        else:
-            return True
+        return True
 
-    def assign_absolute_position(self, x, y):
-        self.absolute_coord.x = x
-        self.absolute_coord.y = y
+    def assign_relative_coord(self, x, y):
+        self.relative_coord.x = x
+        self.relative_coord.y = y
 
+    def valid_relative_coord(self):
+        if self.relative_coord.x is None or self.relative_coord.y is None:
+            return False
+        return True
+
+
+
+
+class Group:
+    def __init__(self, contains, group_id, layer_id, parent_node_id):
+        self.contains = copy.deepcopy(contains)
+        self.group_id = group_id
+        self.layer_id = layer_id
+        self.parent_node_id = parent_node_id
+        # from config , need update later
+        self.text_width, self.text_height = get_default_text_size()
+        self.boarder = self.text_width
+        self.bbox_size = None
+
+    def calc_relative_coord(self, maps):
+        pos_x, pos_y = self.boarder, self.boarder
+        for idx in range(len(self.contains)):
+            node_id = self.contains[idx]
+            maps[node_id].assign_relative_coord(pos_x, pos_y)
+            pos_x += self.text_width+self.boarder
+        pos_y += self.text_height + self.boarder
+        self.bbox_size = (pos_x, pos_y)
+        pass
